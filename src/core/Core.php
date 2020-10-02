feaff57c29ed35571012d15aa214595797cb58b2
Anderson Mendes <anderson2000ano@gmail.com>
2020-10-02 13:44:31 -0300

    support updates via long polling

--- src/core/Core.php
+++ src/core/Core.php
@@ -2,7 +2,7 @@
 
 abstract class Core {
 
-    static public function run(Object $req): void {
+    static public function main(Object $req): void {
         $modules = new Modules();
 
         $module = $modules->getModule($req->message->text);
@@ -14,5 +14,25 @@ abstract class Core {
         $req->message->text = $module->clearMessage($req->message->text);
 
         $module->main(new Telegram($req));
+    }
+
+    static function hook(Object $msg) {
+        self::main($msg);
+    }
+
+    static function run() {
+        $offset = 0;
+        echo "Bot started \n";
+        while(true) {
+            $res = Http::json("https://api.telegram.org/bot".APIKEY."/getUpdates?timeout=30&offset=".$offset);
+            
+            if(count($res->result) > 0){
+                $offset = end($res->result)->update_id + 1;
+
+                foreach($res->result as $msg){
+                    self::main($msg);
+                }
+            }
+        }
     }
 }
